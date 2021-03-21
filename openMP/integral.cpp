6498106b9ca3bfda6323bff262d374af7ee80102
Victoria Dzyuba <victoria2000dzuba@gmail.com>
2021-03-21 18:03:54 +0300

    add integral

---
+++ openMP/integral.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+using namespace std;
+
+double f(double x) {
+    return 4.0 / (1.0 + x * x);
+}
+
+int main(){
+    int counter;
+    double integral;
+    double a = 0.0;
+    double b = 1.0;
+    double h = 0.01;
+
+    double n;
+
+    n = (b - a) / h;
+
+    integral = h * (f(a) + f(b)) / 6.0;
+
+    double alt_integral = 0.0;
+
+    #pragma omp parallel for shared(a,b,h,n) reduction(+:alt_integral)
+        for (int counter = 1; counter<= (int)n; ++counter){
+            alt_integral += h * f(a + h * counter);
+        }
+
+        integral += alt_integral;
+        cout << "Integral = "<<integral<<"\n";
+}
